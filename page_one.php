3ec8793f4cea03df3e6abf78fdc066eddbf8e4dd
msilvana <58324128+msilvana@users.noreply.github.com>
2021-11-12 16:53:20 +0700

    menambahkan vscode dan kolaborasi

--- page_one.php
+++ page_one.php
@@ -3,4 +3,6 @@
 echo "Hai, guys! Bangga bgt bisa ngetik ini :D ";
 echo "Resolusi gw buat GIT ini :";
 echo "1. Konsisten belajar";
+echo "2. Cobain pake vsCode editor";
+echo "3. Cobain kolaborasi dengan aku Git lain";
 ?>
