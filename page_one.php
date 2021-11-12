ae20bd04fe12f83317a044a110eee98d060b1d39
msilvana <58324128+msilvana@users.noreply.github.com>
2021-11-12 16:54:33 +0700

    hapus vscode dan tambah vloog

--- page_one.php
+++ page_one.php
@@ -2,7 +2,7 @@
 
 echo "Hai, guys! Bangga bgt bisa ngetik ini :D ";
 echo "Resolusi gw buat GIT ini :";
-echo "1. Konsisten belajar";
-echo "2. Cobain pake vsCode editor";
-echo "3. Cobain kolaborasi dengan aku Git lain";
+echo "1. Konsisten belajar"; 
+echo "2. Cobain kolaborasi dengan aku Git lain";
+echo "3. Membuat vloog";
 ?>
